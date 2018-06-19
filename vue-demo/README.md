24ab296c12c095227d8fbd2c49ca0dbffe0cf22f
zll18314831310 <18314831310@163.com>
2018-06-19 14:44:41 +0800

    vue demo学习

--- vue-demo/README.md
+++ vue-demo/README.md
@@ -1,4 +1,4 @@
-# vue_mode
+# vue-demo
 
 > A Vue.js project
 
@@ -16,9 +16,6 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
-
-# run unit tests
-npm run unit
 
 # run e2e tests
 npm run e2e
---
+++ vue-demo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <input type="text" v-model="msg"/>
+    <p>{{msg}}</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      msg:''
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
