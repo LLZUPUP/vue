f38af40cf5c8034132da1962a437e688be015b12
zll18314831310 <18314831310@163.com>
2018-06-25 21:38:39 +0800

    vue element-ui需诶下

---
+++ element-ui/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+      <el-button @click="doLoading" :loading="isLoading" icon="el-icon-edit" type="success"></el-button>
+      <el-button icon="el-icon-edit" type="warning">提交</el-button>
+      <el-button loading :disabled="disabled" type="danger">提交</el-button>
+      <el-button :disabled="disabled" type="primary">提交</el-button>
+      <el-button :disabled="disabled" type="info">提交</el-button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ElButton from './components/ElButton'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      disabled: false,
+      isLoading: false
+    }
+  },
+  components: {
+    "el-button": ElButton
+  },
+  mounted() {
+    setTimeout(()=>{
+      this.disabled = true;
+    },2000)
+  },
+  methods:{
+    doLoading() {
+      // return this.isLoading = true
+      console.log('aadsd')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ element-ui/src/components/ElButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="el-button" :class="[
+    type?'el-button--'+type : '',{'is-disabled':buttonDisabled,'isloading':loading}
+    ]" :disabled="buttonDisabled" >
+        <i class="el-icon-loading" v-if="loading"></i>
+        <i v-if="icon&&!loading" :class="icon"></i>
+        <slot></slot>
+    </button>
+</template>
+<style>
+
+</style>
+
+<script>
+export default {
+    name: 'ElButton',
+    props: {
+        disabled: Boolean,
+        type: {
+            type: String,
+            default: 'default'
+        },
+        loading: Boolean,
+        icon: {
+            type:String,
+            default: ''
+        }
+    },
+    computed: {
+        buttonDisabled() {
+            return this.disabled;
+        }
+    }
+}
+</script>
