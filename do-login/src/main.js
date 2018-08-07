b92f7857e76e428b81b1cd6641a5669ecd2d0351
zll18314831310 <18314831310@163.com>
2018-08-07 19:04:25 +0800

    do-login vue全栈

--- do-login/src/main.js
+++ do-login/src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import axios from './axios'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  axios,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
--- do-login/src/router/index.js
+++ do-login/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,63 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Hello',
+      component(resolve) {
+        require.ensure(
+          ['@/components/Hello.vue'],()=> {
+            resolve(require('@/components/Hello.vue'))
+          }
+        )
+      },
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component(resolve) {
+        require.ensure(
+          ['@/components/Login.vue'],()=> {
+            resolve([require('@/components/Login.vue')])
+          }
+        )
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component(resolve) {
+        require.ensure(['@/components/Register.vue'],()=> {
+          resolve([require('@/components/Register.vue')])
+        })
+      }
+    },
+    {
+      path: '*',
+      name: 'register',
+      component(resolve) {
+        require.ensure(['@/components/404.vue'],()=> {
+          resolve([require('@/components/404.vue')])
+        })
+      },
+      hidden: true
     }
   ]
+})
+
+router.beforeEach((to, from, next)=> {
+  let token = localStorage.getItem('token')
+  if(to.meta.requireAuth) {
+    if(token) {
+      next()
+    }else {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    }
+  }else {
+    next()
+  }
 })
